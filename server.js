6d13da3dd893ed9eaa51975b688c4766e882d571
Deepesh-Sabran <deepeshsabran@gmail.com>
2023-11-27 12:09:39 +0530

    new commit

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const http = require("http");
 // declaring port
 const port = 8081;
 
+const toDoList = ["Learn", " apply things", " succed"];
+
 http.createServer((req, res) => {
-    const {method, url} = req;
-    if(method === "GET") {
-        res.writeHead(200, {"content-type":"text/html"});
-        res.write(toDoList.toString());
-        res.end();
-    }
+    res.writeHead(200, {"content-type":"text/html"});
+    res.write(toDoList.toString());
+    res.end();
 }).listen(port, () => {
     console.log(`Node JS server started, running on port: ${port}`);
 });
